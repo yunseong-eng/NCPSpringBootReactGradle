3bf10fa84d268a8f8fa6b1d634968b8475b7b9f7
윤성 <yunseong0220@hanmail.net>
2024-11-15 14:51:01 +0900

    1

---
+++ src/main/frontreact/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Spring Boot + React + NCP</h1> 
+      <h3>카운트 : 3</h3>
+      <img src="./image/spon.png" alt="스폰지밥" />
+    </div>
+  );
+};
+
+export default App;
